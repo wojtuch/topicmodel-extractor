cc54a3fd32c33d6d6b6fe8896d02da66d228d519
Wojciech Lukasiewicz <wojciech.lukasiewicz@fokus.fraunhofer.de>
2016-05-26 17:33:54 +0200

    added skeleton for parsing commandline opts

---
+++ topic-modeller/src/main/java/org/dbpedia/topics/CmdLineOpts.java
@@ -0,0 +1,18 @@
+package org.dbpedia.topics;
+
+import org.apache.commons.cli.Options;
+
+/**
+ * Created by wlu on 26.05.16.
+ */
+public class CmdLineOpts {
+
+    static {
+
+    }
+
+    private Options options = new Options();
+
+    public CmdLineOpts() {
+    }
+}
