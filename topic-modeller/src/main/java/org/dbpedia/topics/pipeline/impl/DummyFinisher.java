f72ccecf8c53f43d821d8f91a212fa83c303a880
Wojciech Lukasiewicz <wojciech.lukasiewicz@fokus.fraunhofer.de>
2016-06-09 15:58:22 +0200

    added a dummy finisher

---
+++ topic-modeller/src/main/java/org/dbpedia/topics/pipeline/impl/DummyFinisher.java
@@ -0,0 +1,14 @@
+package org.dbpedia.topics.pipeline.impl;
+
+import org.dbpedia.topics.dataset.models.Dataset;
+import org.dbpedia.topics.pipeline.PipelineFinisher;
+
+/**
+ * Created by wlu on 09.06.16.
+ */
+public class DummyFinisher implements PipelineFinisher {
+    @Override
+    public void finishPipeline(Dataset dataset) {
+
+    }
+}
