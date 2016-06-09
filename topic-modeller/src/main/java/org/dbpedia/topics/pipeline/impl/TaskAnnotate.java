9ad56bc420144be5a256b7d8598ff8ea4c0acd9d
Wojciech Lukasiewicz <wojciech.lukasiewicz@fokus.fraunhofer.de>
2016-06-09 15:43:28 +0200

    added some task implementations

---
+++ topic-modeller/src/main/java/org/dbpedia/topics/pipeline/impl/TaskAnnotate.java
@@ -0,0 +1,30 @@
+package org.dbpedia.topics.pipeline.impl;
+
+import org.dbpedia.topics.dataset.models.Dataset;
+import org.dbpedia.topics.dataset.models.Instance;
+import org.dbpedia.topics.pipeline.PipelineTask;
+import org.dbpedia.utils.annotation.SpotlightAnnotator;
+import org.dbpedia.utils.annotation.models.SpotlightAnnotation;
+
+/**
+ * Created by wlu on 09.06.16.
+ */
+public class TaskAnnotate implements PipelineTask {
+
+    private SpotlightAnnotator spotlightAnnotator;
+
+    public TaskAnnotate(String spotlightEndpoint) {
+        spotlightAnnotator = new SpotlightAnnotator(spotlightEndpoint);
+    }
+
+    @Override
+    public Dataset start(Dataset dataset) {
+
+        for (Instance instance : dataset) {
+            SpotlightAnnotation annotation = spotlightAnnotator.annotate(instance.getText(), 0, 0.5);
+            instance.setSpotlightAnnotation(annotation);
+        }
+
+        return dataset;
+    }
+}
